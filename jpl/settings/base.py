55a059e16e7640bc7e0dfd99c93ae31589db5065
Tom Dyson <tom@torchbox.com>
2020-04-15 08:33:14 +0100

    Update base URL

--- jpl/settings/base.py
+++ jpl/settings/base.py
@@ -151,4 +151,4 @@ WAGTAIL_SITE_NAME = "JPL"
 
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
-BASE_URL = "http://example.com"
+BASE_URL = "https://jpl-poc.torchbox.com"
