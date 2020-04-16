c21a99c1eeb64727f81d38b67a397b76a63143bf
Mitchel Cabuloy <mixxorz@gmail.com>
2020-04-16 15:05:31 +0800

    Add custom embed block
    For caption and credits

--- jpl/news/blocks.py
+++ jpl/news/blocks.py
@@ -1,11 +1,21 @@
 from django.core.exceptions import ValidationError
 from wagtail.core import blocks
 from wagtail.documents.blocks import DocumentChooserBlock
-from wagtail.embeds.blocks import EmbedBlock
+from wagtail.embeds.blocks import EmbedBlock as DefaultEmbedBlock
 from wagtail.images.blocks import ImageChooserBlock
 
-from wagtail_content_import.mappers.converters import ImageConverter, RichTextConverter 
+from wagtail_content_import.mappers.converters import ImageConverter, RichTextConverter
 from wagtail_content_import.mappers.streamfield import StreamFieldMapper
+
+
+class EmbedBlock(blocks.StructBlock):
+    embed = DefaultEmbedBlock()
+    caption = blocks.TextBlock(required=False)
+    credit = blocks.CharBlock(required=False, max_length=255)
+
+    class Meta:
+        icon = "media"
+        template = "blocks/embed_block.html"
 
 
 class ImageBlock(blocks.StructBlock):
